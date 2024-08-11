5f5c0076ce608be7225074ba1f95c03cc36af5c0
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-12 01:21:50 +0300

    create account

--- Pages/NewAccount.xaml.cs
+++ Pages/NewAccount.xaml.cs
@@ -1,17 +1,78 @@
 using CommunityToolkit.Maui.Views;
-
+using DsaccoAPP.Model.Mapper;
+using Newtonsoft.Json;
+using System.Text;
 namespace DsaccoAPP.Pages;
 
 public partial class NewAccount : Popup
 {
-	public NewAccount()
+   
+
+    decimal currentBalance;
+    decimal openingAmount;
+    string accountDescription;
+    string baseUrl = "https://localhost:7231/api/Account/openaccount";
+    static HttpClient client;
+    public NewAccount()
 	{
 		InitializeComponent();
-		
-	}
+        BindingContext = this;
+        client = new HttpClient
+        {
+            BaseAddress = new Uri(baseUrl)
+        };
+
+    }
+    public decimal CurrentBalance
+    {
+        get { return currentBalance; }
+        set
+        {
+            currentBalance = value;
+            OnPropertyChanged();
+        }
+    }
+    public decimal OpeningAmount
+    {
+        get { return openingAmount; }
+        set
+        {
+            openingAmount = value;
+            OnPropertyChanged();
+        }
+    }
+    public string AccountDescription
+    {
+        get { return accountDescription; }
+        set { accountDescription = value; OnPropertyChanged(); }
+    }
+
+  
 
     private void closepopup(object sender, EventArgs e)
     {
 		Close();
     }
+
+    private async void CreateAccount(object sender, EventArgs e)
+    {
+        AccountModel AccountModel = new() { AccountDescription = accountType.SelectedItem.ToString(), OpeningAmount = OpeningAmount, CurrentBalance = CurrentBalance };
+        var json = JsonConvert.SerializeObject(AccountModel);
+        var postData = new StringContent(json, Encoding.UTF8, "application/json");
+        var res = await client.PostAsync(baseUrl, postData);
+        if (!res.IsSuccessStatusCode)
+        {
+            var route = $"{nameof(Register)}";
+            await Shell.Current.GoToAsync(route);
+
+        }
+        else
+        {
+            var route = $"{nameof(Login)}";
+            await Shell.Current.GoToAsync(route);
+            Close();
+        }
+
+
+        }
 }
