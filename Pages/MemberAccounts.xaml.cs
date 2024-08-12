60c55e554d1d36b2ebbc71552345c6faf5e4f07f
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-12 13:31:30 +0300

    add observable collection

--- Pages/MemberAccounts.xaml.cs
+++ Pages/MemberAccounts.xaml.cs
@@ -1,10 +1,25 @@
 namespace DsaccoAPP.Pages;
 using CommunityToolkit.Maui.Views;
+using DsaccoAPP.Model.BaseClasses;
+using System.Collections.ObjectModel;
 
 public partial class MemberAccounts : Popup
 {
-	public MemberAccounts()
+    public ObservableCollection<Account> memberAccounts { get; set; } = new ObservableCollection<Account>();
+
+    string baseUrl = "https://localhost:7231/api/Account/openaccount";
+    static HttpClient client;
+    decimal CurrentBalance;
+   decimal InitialDeposit ;
+  decimal OpeningAmount;
+    string OpeningDate;
+    public MemberAccounts()
 	{
 		InitializeComponent();
-	}
+        client = new HttpClient
+        {
+            BaseAddress = new Uri(baseUrl)
+        };
+
+    }
 }
