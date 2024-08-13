936905f3b5d7049abc2cda6f2afd82103c1830a9
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-13 13:50:19 +0300

    add transaction class

---
+++ Model/Mapper/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsaccoAPP.Model.Mapper
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public string TransactionDate { get; set; } = string.Empty;
+
+
+
+    }
+}
--- Pages/WithdrawDetails.xaml.cs
+++ Pages/WithdrawDetails.xaml.cs
@@ -2,12 +2,20 @@ namespace DsaccoAPP.Pages;
 using CommunityToolkit.Maui.Views;
 using System.Collections.ObjectModel;
 using DsaccoAPP.Model.BaseClasses;
+using DsaccoAPP.Model.Mapper;
 
 public partial class WithdrawDetails : Popup
 {
-    public ObservableCollection<Withdraw> CashWithdraws{ get; set; } = new ObservableCollection<Withdraw>();
+    public ObservableCollection<Transaction> CashWithdraws{ get; set; } = new ObservableCollection<Transaction>();
+    string baseUrl = "https://localhost:7231/api/Account/withdraws";
+    static HttpClient client;
+    public List<Withdraw> MemberWithdraws { get; set; }
     public WithdrawDetails()
 	{
 		InitializeComponent();
-	}
+        client = new HttpClient
+        {
+            BaseAddress = new Uri(baseUrl)
+        };
+    }
 }
