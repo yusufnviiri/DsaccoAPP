88cebe8c0643fad518248c97968a1892fba62f94
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-15 10:20:37 +0300

    add stock

---
+++ Pages/BuyShares.xaml.cs
@@ -0,0 +1,14 @@
+namespace DsaccoAPP.Pages;
+using CommunityToolkit.Maui.Views;
+using System.Collections.ObjectModel;
+using DsaccoAPP.Model.BaseClasses;
+using DsaccoAPP.Model.Mapper;
+using Newtonsoft.Json;
+
+public partial class BuyShares : Popup
+{
+	public BuyShares()
+	{
+		InitializeComponent();
+	}
+}
