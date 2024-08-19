294e0538d106d9557d3896ce9b8c2c8522f787d8
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-19 11:01:45 +0300

    add reject button

--- Pages/AllLoans.xaml.cs
+++ Pages/AllLoans.xaml.cs
@@ -5,10 +5,12 @@ using DsaccoAPP.Model.BaseClasses;
 using DsaccoAPP.Model.Mapper;
 using Newtonsoft.Json;
 using System.Text;
+using DsaccoAPP.Model;
 
 public partial class AllLoans : ContentPage
 {
     string loanUrl = "https://localhost:7231/api/Loan/allloans";
+    string loanUpdateUrl = "https://localhost:7231/api/Loan/approveloan";
     static HttpClient client;
     public ObservableCollection<Loan> LoansBindableList { get; set; }= new ObservableCollection<Loan>();
     private IEnumerable<Loan> loansfromDb {  get; set; }= new List<Loan>();
@@ -39,7 +41,11 @@ public partial class AllLoans : ContentPage
                 item.loanCompletiton = "False";
 
             }
-            LoansBindableList.Add(item);
+            if (LoansBindableList.Count()==res.Count()) { return; }
+            else
+            {
+                LoansBindableList.Add(item);
+            }
 
 
         
@@ -58,14 +64,29 @@ public partial class AllLoans : ContentPage
 
     private async void GetallLoans(object sender, EventArgs e)
     {
+
+        LoansBindableList.Clear();
         var response = await client.GetStringAsync(loanUrl);
         var res = JsonConvert.DeserializeObject<IEnumerable<Loan>>(response);
         loansfromDb = res;
         foreach (var item in loansfromDb)
         {
-            LoansBindableList.Add(item);
-        }
+            if (item.IsCompleted)
+            {
+                item.loanCompletiton = "True";
+            }
+            else
+            {
+                item.loanCompletiton = "False";
 
+            }
+            if (LoansBindableList.Count() == res.Count()) { return; }
+            else
+            {
+                LoansBindableList.Add(item);
+            }
+
+        }
     }
 
     private async void BackToHome(object sender, EventArgs e)
@@ -74,22 +95,44 @@ public partial class AllLoans : ContentPage
 
     }
 
-    private void OnApproveLoan(object sender, EventArgs e)
+    private async Task UpdateLoanStatus(ParamId paramId)
+    {
+        var json = JsonConvert.SerializeObject(paramId);
+        var postData = new StringContent(json, Encoding.UTF8, "application/json");
+        var res = await client.PostAsync(loanUpdateUrl, postData);
+        if (!res.IsSuccessStatusCode)
+        {
+            var route = $"{nameof(Login)}";
+            await Shell.Current.GoToAsync(route);
+        }
+        else
+        {
+            var route = $"{nameof(Register)}";
+            await Shell.Current.GoToAsync(route);
+        }
+
+    }
+
+    private async void OnApproveLoan(object sender, EventArgs e)
     {
         var button = sender as Button;
         var selectedItem = button.BindingContext as Loan;
         if (selectedItem != null)
         {
             DisplayAlert("Item Selected", selectedItem.LoanType.Description + " "+ selectedItem.LoanId, "OK");
+            ParamId paramId = new ParamId() { RefId=selectedItem.LoanId,Status="Approved"};
+            await UpdateLoanStatus(paramId);
         }
     }
-    private void OnRejectLoan(object sender, EventArgs e)
+    private async void OnRejectLoan(object sender, EventArgs e)
     {
         var button = sender as Button;
         var selectedItem = button.BindingContext as Loan;
         if (selectedItem != null)
         {
             DisplayAlert("Item Selected", selectedItem.User.LastName + " " + selectedItem.User.FirstName, "OK");
+            ParamId paramId = new ParamId() { RefId = selectedItem.LoanId, Status = "Rejected" };
+            await UpdateLoanStatus(paramId);
         }
     }
 }
