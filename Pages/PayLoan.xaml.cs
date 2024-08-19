c85ba18ebf0a77bd8f15a0485149c87fd9758fd8
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-19 12:06:48 +0300

    ADD PAYLOAN

---
+++ Pages/PayLoan.xaml.cs
@@ -0,0 +1,14 @@
+namespace DsaccoAPP.Pages;
+using CommunityToolkit.Maui.Views;
+using System.Collections.ObjectModel;
+using DsaccoAPP.Model.BaseClasses;
+using DsaccoAPP.Model.Mapper;
+using Newtonsoft.Json;
+using System.Text;
+public partial class PayLoan : Popup
+{
+	public PayLoan()
+	{
+		InitializeComponent();
+	}
+}
