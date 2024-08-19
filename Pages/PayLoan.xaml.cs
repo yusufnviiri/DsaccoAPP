f9d695c90863c94c609eff143875ac56cd475f87
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-08-19 12:18:10 +0300

    add loan payment

--- Pages/PayLoan.xaml.cs
+++ Pages/PayLoan.xaml.cs
@@ -7,7 +7,14 @@ using Newtonsoft.Json;
 using System.Text;
 public partial class PayLoan : Popup
 {
-	public PayLoan()
+    public List<LoanType> LoanTypesList { get; set; } = new List<LoanType>();
+    string loanPaymentUrl = "https://localhost:7231/api/Loan/loanpayment";
+    LoanPaymentModel loanPayment = new LoanPaymentModel();
+    int loanId;
+    static HttpClient client;
+    double sharesOwned;
+    decimal amountPaid;
+    public PayLoan()
 	{
 		InitializeComponent();
 	}
